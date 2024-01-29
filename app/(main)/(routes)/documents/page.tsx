81a4775599b9b14c2536c1ba17307bfd85552917
lsy8658 <socszz@gmail.com>
2024-01-29 16:58:07 +0900

    user logout 기능 작성

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import Image from "next/image";
 import React from "react";
-
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 export default function DocumentsPage() {
-  return <div>This is a protected DocumentsPage</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium line-clamp-1">
+        {user?.username}&apos;님 Snote에 방문해 주셔서 감사합니다.
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 }
---
+++ app/(main)/_components/user-item.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from "react";
+import { ChevronsLeftRight } from "lucide-react";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
+/*
+    특정 컴포넌트가 "asChild"를 통해 
+    자식 컴포넌트를 받아들이는 경우, 
+    해당 자식 컴포넌트는 부모 컴포넌트의 
+    일부 동작이나 스타일을 상속받을 수 있습니다.
+*/
+export default function UserItem() {
+  const { user } = useUser();
+  console.log(user);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full
+        hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span className="text-start font-medium line-clamp-1">
+              {user?.username}&apos;s Snote
+            </span>
+          </div>
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-80"
+        align="start"
+        alignOffset={11}
+        forceMount
+      >
+        <div className="flex flex-col space-y-4 p-2">
+          <p className="text-xs font-medium leading-none text-muted-foreground">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+          <div className="flex items-center gap-x-2 ">
+            <div className="p-1">
+              <Avatar>
+                <AvatarImage
+                  src={user?.imageUrl}
+                  className="rounded-full object-cover w-8 h-8"
+                />
+              </Avatar>
+            </div>
+            <div className="space-y-1 line-clamp-1">
+              <p className="text-sm ">{user?.username}&apos;</p>
+            </div>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          asChild
+          className="w-full cursor-pointer text-muted-foreground"
+        >
+          <SignOutButton>Log out</SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
